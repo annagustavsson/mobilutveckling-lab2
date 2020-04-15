bc24b75797e37d6927b6aac50210fa487e04816f
annagu3 <annagu3@kth.se>
2020-04-15 15:39:42 +0200

    Setup for subclasses with text

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const loadPage = (page) => {
-  document.querySelector("#index").close();
-  document
-    .querySelector("#breathing")
-    .bringPageTop(page, { animation: "fade" });
-};
+document.addEventListener("init", function (event) {
+  var page = event.target;
+
+  console.log("i app.js");
+
+  if (page.id === "main") {
+    console.log("i if-satsen");
+
+    page.querySelector("#push-breathing").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("breathing.html", { data: { title: "Page 2" } });
+    };
+
+    page.querySelector("#push-yoga").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("yoga.html", { data: { title: "Page 2" } });
+    };
+
+    page.querySelector("#push-relaxation").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("relaxation.html", { data: { title: "Page 2" } });
+    };
+
+    page.querySelector("#push-about").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("relaxation.html", { data: { title: "Page 2" } });
+    };
+  }
+
+  //   else if (page.id === "page2") {
+  //     page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+  //   }
+});
