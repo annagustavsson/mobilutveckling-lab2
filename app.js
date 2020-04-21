49de1d80befcfc84519c4fefb6c87aa118db3da8
Kristina Andersson <kristina.andersson@mail.ru>
2020-04-21 20:35:08 +0200

    Exercise 3, 4: more styling + created dialog

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 document.addEventListener("init", function (event) {
-  var page = event.target;
+	const page = event.target;
 
-  console.log("i app.js");
+	if (page.id === "main") {
+		console.log("i if-satsen");
 
-  if (page.id === "main") {
-    console.log("i if-satsen");
+		page.querySelector("#push-breathing").onclick = function () {
+			document
+				.querySelector("#myNavigator")
+				.pushPage("breathing.html", { data: { title: "Page 2" } });
+		};
 
-    page.querySelector("#push-breathing").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("breathing.html", { data: { title: "Page 2" } });
-    };
+		page.querySelector("#push-yoga").onclick = function () {
+			document
+				.querySelector("#myNavigator")
+				.pushPage("yoga.html", { data: { title: "Page 2" } });
+		};
 
-    page.querySelector("#push-yoga").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("yoga.html", { data: { title: "Page 2" } });
-    };
+		page.querySelector("#push-relaxation").onclick = function () {
+			document
+				.querySelector("#myNavigator")
+				.pushPage("relaxation.html", { data: { title: "Page 2" } });
+		};
 
-    page.querySelector("#push-relaxation").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("relaxation.html", { data: { title: "Page 2" } });
-    };
-
-    page.querySelector("#push-about").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("about.html", { data: { title: "Page 2" } });
-    };
-  }
-
-  //   else if (page.id === "page2") {
-  //     page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
-  //   }
+		page.querySelector("#push-about").onclick = function () {
+			document
+				.querySelector("#myNavigator")
+				.pushPage("about.html", { data: { title: "Page 2" } });
+		};
+	}
 });
+
+const showTemplateDialog = () => {
+	const dialog = document.getElementById("my-dialog");
+
+	if (dialog) {
+		dialog.show();
+	} else {
+		ons.createElement("dialog.html", { append: true }).then(function (dialog) {
+			dialog.show();
+		});
+	}
+};
+
+const hideDialog = (id) => {
+	document.getElementById(id).hide();
+};
